b03663260c79322a9fc0b3075095a833e30e19f9
Ka Thas <kavinthas@gmail.com>
2026-02-08 18:51:07 +0100

    Mower sprites

--- src/Player.py
+++ src/Player.py
@@ -2,7 +2,7 @@ import pygame
 
 from .options import *
 from .Projectile import Projectile
-from .assets import *
+from .assets import MOWER_UP, MOWER_DOWN, MOWER_LEFT, MOWER_RIGHT
 
 class Player:
     def __init__(self):
@@ -10,7 +10,7 @@ class Player:
         self.y = 640
         self.size = (PLAYER_SIZE, PLAYER_SIZE)
         self.color = BLUE
-        self.image = pygame.transform.scale(MOWER_IMAGE, self.size)
+        self.image = pygame.transform.scale(MOWER_UP, self.size)
         self.speed = PLAYER_SPEED
         self.length = 1000 
         self.rect = pygame.Rect(self.x, self.y, self.size[0], self.size[1])
@@ -21,8 +21,22 @@ class Player:
     def draw(self, direction, screen):
 
         # Rotate the image based on the direction
-        angle = direction.angle_to(pygame.Vector2(0, -1))
-        rotated_image = pygame.transform.rotate(self.image, angle)
+        # Use up or down if diagonal
+        if direction.x != 0 and direction.y != 0:
+            if direction.y < 0:
+                self.image = MOWER_UP
+            else:
+                self.image = MOWER_DOWN
+        else:
+            if direction.x > 0:
+                self.image = MOWER_RIGHT
+            elif direction.x < 0:
+                self.image = MOWER_LEFT
+            elif direction.y < 0:
+                self.image = MOWER_UP
+            elif direction.y > 0:
+                self.image = MOWER_DOWN
+
         self.rect = pygame.Rect(self.x, self.y, self.size[0], self.size[1])
-        screen.blit(rotated_image, self.rect)
+        screen.blit(self.image, self.rect)
 
